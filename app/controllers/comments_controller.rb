db744e0d15ea8c970df13ee7336934372a7eb172
= <=>
2021-11-03 16:31:59 +0700

    menambah create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,17 @@
 class CommentsController < ApplicationController
 	def create
-		
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new(comment_params)
+		@comment.user = current_user
+		@comment.post = @post
+		@comment.save
+
+		redirect_to post_path(@post)
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:comment)
 	end
 end
