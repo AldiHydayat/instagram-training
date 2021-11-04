1ee5b60aff2a6c027caf3f7dc339e82c10f3a434
Aldi Hidayat <aldyh128@gmail.com>
2021-11-04 16:39:47 +0700

    memperbaiki validasi authentikasi

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	before_action: :authenticate_user!, only: [:create]
+	before_action :authenticate_user!, only: [:create]
 
 	def create
 		@post = Post.find(params[:post_id])
