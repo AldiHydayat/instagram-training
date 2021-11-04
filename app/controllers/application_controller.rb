be5b319eeb2324c11bf4b676bcb5feab16249e2a
Aldi Hidayat <aldyh128@gmail.com>
2021-11-04 13:17:09 +0700

    memperbaiki display image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :username, :password])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :name, :username, :password])
   end
 end
